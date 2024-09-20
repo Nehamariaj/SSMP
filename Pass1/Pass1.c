176e1fe2fddf7e9b71fdef78efc6c5bff5c47b04
Neha Maria Joji <145547103+Nehamariaj@users.noreply.github.com>
2024-09-20 12:26:17 +0530

    Pass1.c

--- Pass1/Pass1.c
+++ Pass1/Pass1.c
@@ -2,13 +2,14 @@
 #include<string.h>
 #include<stdlib.h>
 void main(){
-FILE *input, *optab, *symtab, *intermediate;
+FILE *input, *optab, *symtab,*intermediate,*prgmlength;
 int locctr, start, flag, program_length;
 char opcode[20], operand[20], label[20], mneumonic[20], code[20];
 input = fopen("input.txt", "r");
 optab = fopen("optab.txt", "r");
 symtab = fopen("symtab.txt", "w");
 intermediate = fopen("intermediate.txt", "w");
+prgmlength=fopen("prgmlength.txt","w");
 if (input == NULL || optab == NULL || symtab == NULL || intermediate == NULL) { 
 printf("Error opening file!\n");
 exit(0);
@@ -17,7 +18,7 @@ fscanf(input, "%s %s %s", label, opcode, operand);
 if (strcmp(opcode, "START") == 0) {
 start = atoi(operand);
 locctr = start;
-fprintf(intermediate, "     %s\t%s\n", opcode, operand);
+fprintf(intermediate, "     %-7s%-7s%-7s\n",label,opcode, operand);
 } 
 else {
 locctr = 0;
@@ -25,9 +26,9 @@ locctr = 0;
 printf("LOCCTR:\n%d\n",locctr);
 fscanf(input, "%s %s %s", label, opcode, operand);
 while (strcmp(opcode, "END") != 0) {
-fprintf(intermediate, "%d\t%s\t%s\t%s\n", locctr, label, opcode, operand);
+fprintf(intermediate, "%d %-7s%-7s%-7s\n", locctr, label, opcode, operand);
 flag = 0;
-if (strcmp(label, "**") != 0) {
+if (strcmp(label, "*****") != 0) {
 fprintf(symtab, "%s\t%d\n", label, locctr);
 } 
 if (strcmp(opcode, "WORD") == 0) {
@@ -49,11 +50,13 @@ flag = 1;
 printf("%d\n",locctr);
 fscanf(input, "%s %s %s", label, opcode, operand);
 }
-fprintf(intermediate, "%d\t%s\t%s\t%s\n",locctr,label, opcode, operand);
+fprintf(intermediate, "%d %-7s%-7s%-7s\n",locctr,label, opcode, operand);
 program_length = locctr - start;
-printf("Program Length = %d\n", program_length);
+printf("%d\n", program_length);
+fprintf(prgmlength,"%d",program_length);
 fclose(input);
 fclose(intermediate);
 fclose(symtab);
 fclose(optab);
+fclose(prgmlength);
 }
